ff30fb44aa9953c70732404e9f474fc978000ff1
Niaz Islam <niaz_s4@hotmail.com>
2026-02-14 21:38:07 +0600

    Add updated Practise_1.sql with all assignments

--- maven-movies-analysis/Practise_1.sql
+++ maven-movies-analysis/Practise_1.sql
@@ -2,16 +2,8 @@
 
 
 -- ASSIGNMENT 1: Subqueries in the SELECT clause
-
--- View the products table
-
-
--- View the average unit price
-
-
 -- Return the product id, product name, unit price, average unit price,
 -- and the difference between each unit price and the average unit price
-
 
 -- Order the results from most to least expensive
 SELECT product_id, product_name, unit_price,
@@ -19,7 +11,6 @@ SELECT product_id, product_name, unit_price,
 unit_price - (SELECT avg(unit_price) FROM products) AS avg_price_diff
 FROM products
 ORDER BY unit_price DESC
-
 
 
 -- ASSIGNMENT 2: Subqueries in the FROM clause
@@ -61,7 +52,6 @@ FROM products
 WHERE factory = "Wicked Choccy's" AND unit_price IS NULL
 
 
-
 -- Return products where the unit price is less than the unit price of all products from Wicked Choccy's
 SELECT *
 FROM products
@@ -69,6 +59,66 @@ WHERE unit_price < ALL
 (SELECT  unit_price
 FROM products
 WHERE factory = "Wicked Choccy's")
+
+-- ASSIGNMENT 4: CTEs
+
+-- View the orders and products tables
+SELECT*
+FROM products
+
+SELECT *
+FROM orders
+
+-- Calculate the amount spent on  each order
+
+-- Return all orders over $200
+WITH order_wise_cost AS (SELECT  o.order_id, sum(p.unit_price * o.units) AS cost
+FROM orders AS o INNER JOIN  products AS p
+ON o.product_id = p.product_id
+GROUP BY   o.order_id)
+SELECT *
+FROM order_wise_cost
+WHERE cost >200
+ORDER BY cost DESC
+
+
+-- Return the number of orders over $200
+WITH order_total AS (SELECT  o.order_id, count(o.order_id) AS total_orders, sum(p.unit_price * o.units) AS cost
+FROM orders AS o INNER JOIN  products AS p
+ON o.product_id = p.product_id
+GROUP BY   o.order_id)
+SELECT order_total.order_id, total_orders
+FROM order_total
+WHERE cost >200
+ORDER BY total_orders DESC
+
+-- ASSIGNMENT 5: Multiple CTEs
+
+-- Copy over Assignment 2 (Subqueries in the FROM clause) solution
+
+-- Return the factories, product names from the factory and number of products produced by each factory
+SELECT p1.factory, p1.product_name, prod_count
+FROM products AS p1
+INNER JOIN 
+(SELECT factory, COUNT(product_name) AS prod_count
+FROM products
+GROUP BY factory) AS p2
+ON p1.factory = p2.factory
+
+
+
+-- Rewrite the Assignment 2 subquery solution using CTEs instead
+
+WITH p1 AS (SELECT factory, product_name
+			FROM products),
+	p2 AS (SELECT factory, COUNT(product_name) AS prod_count
+			FROM products
+			GROUP BY factory) 
+SELECT p1.factory, p1.product_name, prod_count
+FROM p1 INNER JOIN p2
+ON p1.factory = p2.factory
+
+
 
 
 
